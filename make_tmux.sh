69ed5dc5f7d2bfd91262c2d7023a9e39b744ff8f
Kruchinin-v <vladislav97kr@gmail.com>
2020-05-26 14:57:46 +0300

    Добавил сборку tmux

--- make_tmux.sh
+++ make_tmux.sh
@@ -9,6 +9,7 @@ mkdir /usr/local/soft
 
 # DOWNLOAD SOURCES FOR LIBEVENT AND MAKE AND INSTALL
 curl -Lk https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz -o /usr/local/soft/libevent-stable.tar.gz
+cd /usr/local/soft
 tar -xf libevent-stable.tar.gz
 cd libevent-*
 ./configure --prefix=/usr/local
@@ -18,6 +19,7 @@ make install
 # DOWNLOAD SOURCES FOR TMUX AND MAKE AND INSTALL
 
 curl -Lk https://github.com/tmux/tmux/releases/download/2.8/tmux-2.8.tar.gz /usr/local/soft/tmux.tar.gz
+cd /usr/local/soft
 tar -xf tmux.tar.gz
 cd tmux-*
 LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr/local
